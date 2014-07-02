0adf300c1801bbf58313a064e31ccc2933fbb0eb
Alex David D'Angeli <alexdangeli1@lara-dangelis-computer.local>
2014-07-02 16:40:36 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,15 +7,17 @@
 - (void)initialize
 {
     // your code here
-}
+character = (Character*)[CCBReader load:@"Character"];
+[physicsNode addChild:character];
 
 -(void)update:(CCTime)delta
-{
+
     // put update code here
-}character = (Character*)[CCBReader load:@"Character"];
-[physicsNode addChild:character];
+    - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+        // this will get called every time the player touches the screen
+    [Character flap];
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
-}[character flap];
+}
 @end
