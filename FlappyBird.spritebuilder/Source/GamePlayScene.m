02a91940296cf1e8f0af100c55a1b66a7d73540a
MGWU <mgwu@lara-dangelis-computer.local>
2014-07-02 18:26:08 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,8 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -19,5 +21,4 @@
     // this will get called every time the player touches the screen
     [character flap];
 }
-
 @end
